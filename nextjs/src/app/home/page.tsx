52e6899f595eef3df937c3ed5280b560f3f7560f
Aakriti khatiwada <aakritikhatiwada9493@gmail.com>
2024-05-05 00:26:49 +0545

    added home page layout

--- nextjs/src/app/home/page.tsx
+++ nextjs/src/app/home/page.tsx
@@ -1,1 +1,15 @@
+import Footer from "@/components/footer/page";
+import Header from "@/components/navbar/page";
+import React from "react";
 
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      This is a Home page
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
